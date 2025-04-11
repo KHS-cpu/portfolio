a952a9590f05c0cb8a72cb27af267eed3c4f1c09
Kaung Htet San <kaunghtetsan0094@gmail.com>
2025-04-11 22:58:16 +0700

    Added the Access Control Allow Origin to allow also from https://kaunghtetsan.tech

--- terraform/modules/back-end/user_count_lambda.py
+++ terraform/modules/back-end/user_count_lambda.py
@@ -6,19 +6,26 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('visitor_count_2')
 
 def lambda_handler(event, context):
-    # Get the current visitor count
+    allowed_origins = ['https://www.kaunghtetsan.tech', 'https://kaunghtetsan.tech']
+    request_origin = event['headers'].get('origin') or event['headers'].get('Origin')
+
+    # Default to first allowed origin if not matched
+    origin_to_use = allowed_origins[0]
+    if request_origin in allowed_origins:
+        origin_to_use = request_origin
+
+    # Get current visitor count
     response = table.get_item(Key={'obj': '0'})
     views = response['Item']['views']
     views = views + 1
 
-    # Update the visitor count in DynamoDB
+    # Update in DynamoDB
     table.put_item(Item={'obj': '0', 'views': views})
 
-    # Return the updated visitor count with CORS headers
     return {
         'statusCode': 200,
         'headers': {
-            'Access-Control-Allow-Origin': 'https://www.kaunghtetsan.tech',  # Or use "*" to allow all
+            'Access-Control-Allow-Origin': origin_to_use,
             'Access-Control-Allow-Methods': 'GET, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
         },
