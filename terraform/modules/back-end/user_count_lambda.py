5061a810f8ec1073e158936f17c70ec869151833
Kaung Htet San <kaunghtetsan0094@gmail.com>
2025-04-11 12:39:58 +0700

    Added the lambda python file

---
+++ terraform/modules/back-end/user_count_lambda.py
@@ -0,0 +1,26 @@
+import json
+import boto3
+from decimal import Decimal
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('visitor_count')
+
+def lambda_handler(event, context):
+    # Get the current visitor count
+    response = table.get_item(Key={'obj': '0'})
+    views = response['Item']['views']
+    views = views + 1
+
+    # Update the visitor count in DynamoDB
+    table.put_item(Item={'obj': '0', 'views': views})
+
+    # Return the updated visitor count with CORS headers
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',  # Or use your domain: 'https://www.kaunghtetsan.tech'
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
+        },
+        'body': json.dumps({'visitor_count': views}, default=str)
+    }
