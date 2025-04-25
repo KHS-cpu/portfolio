d5aefbbf2ecbd36c4d125341dabd4b11a253873a
Kaung Htet San <kaunghtetsan0094@gmail.com>
2025-04-25 15:55:01 +0700

    Update index.js

--- website/index.js
+++ website/index.js
@@ -3,9 +3,9 @@ console.log("Counter element:", counter);
 
 async function updateCounter() {
     try {
-        const apiUrl = window.apiConfig.apiUrl;
-        let response = await fetch(apiUrl);
-        let data = await response.json();
+        const apiUrl = window.apiConfig.apiUrl;  // Fetching the apiUrl from config.js
+        let response = await fetch(apiUrl);  // Sending a GET request to the API Gateway URL
+        let data = await response.json();  // Parsing the response as JSON
 
         console.log("API response:", data);
 
